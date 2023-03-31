a61a2aa60930fe276c6280e413ac48663ec42b4c
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-03-31 09:34:03 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int order) {
+        int answer, count = 0;
+        
+        while(order>0){
+            if(order%10==3 ||order%10==6 ||order%10==9){
+                count++;
+                
+            }
+            order/=10;
+        }
+        answer=count;
+        
+        return answer;
+    }
+}
