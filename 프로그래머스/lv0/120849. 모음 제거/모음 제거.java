2fe77c3f429633066349c2bf426a1c9c739b3144
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-04-15 00:09:54 +0900

    [level 0] Title: 모음 제거, Time: 0.10 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,17 +1,10 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        String[] replace = {"a", "e", "i", "o", "u"};
-        for(int i = 0; i<replace.length; i++) {
-			if( my_string.contains(replace[i])) {
-				answer = my_string.replaceAll(replace[i], "");
-				my_string = answer;
-			} else {
-                answer =my_string;
-            }
-		}
         
+        String result = my_string.replaceAll("[aeiou]","");
+
         
-        return answer;
+        return result;
     }
 }
