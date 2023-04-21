8c05acf0395e03759b0f3fa297a77f5e1e5651a6
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-04-21 10:17:09 +0900

    [level 0] Title: 암호 해독, Time: 3.17 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = ""; 
+        String[] str =cipher.split("");
+        
+        for(int i=0;i<str.length;i++){
+            if((i+1)%code ==0){
+                answer+=str[i];
+            }
+        }
+        System.out.println(answer);
+        
+        return answer;
+    }
+}
