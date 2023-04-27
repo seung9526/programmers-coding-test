554b633f532fed29e652329d2d5f74bbe86764ac
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-04-27 11:54:53 +0900

    [unrated] Title: 배열 조각하기, Time: 0.02 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        
+        int start =0;
+        int end = arr.length;
+        
+        for(int i=0;i<query.length;i++){
+            if(i%2==0){
+                end=start+query[i];
+            }
+            else{
+                start+= query[i];
+            }
+        }
+        
+        if(start>end){
+            return new int[]{-1};
+        }
+        
+        return Arrays.copyOfRange(arr, start, end+1);
+    }
+}
