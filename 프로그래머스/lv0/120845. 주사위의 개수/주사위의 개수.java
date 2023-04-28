7dcc9e996a4efd5aab6f760f16f7a8e30000fb4b
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-04-28 12:18:45 +0900

    [level 0] Title: 주사위의 개수, Time: 0.04 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer, w, h, d = 0;
+        
+        w=box[0]/n;
+        h=box[1]/n;
+        d=box[2]/n;
+        
+        answer = w*h*d;
+        
+        return answer;
+    }
+}
