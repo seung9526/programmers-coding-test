6880f661c9546a240ce7436b31419388f10d7ad6
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-05-03 12:47:52 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.18 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,42 +1,50 @@
-import java.util.*;
-
 class Solution {
-    
+    //        0부터 9까지 좌표 {y,x}
+    int[][] numpadPos = {
+            {3,1}, //0
+            {0,0}, //1
+            {0,1}, //2
+            {0,2}, //3
+            {1,0}, //4
+            {1,1}, //5
+            {1,2}, //6
+            {2,0}, //7
+            {2,1}, //8
+            {2,2}  //9
+    };
+    //초기 위치
+    int[] leftPos = {3,0};
+    int[] rightPos = {3,2};
+    String hand;
     public String solution(int[] numbers, String hand) {
-        
+        this.hand = (hand.equals("right")) ? "R" : "L";
+
         String answer = "";
-        int left =10;
-        int right =12;
-        
-        for(int tmp: numbers){
-            if(tmp==1||tmp==4||tmp==7){
-                answer+="L";
-                left = tmp;
-            }else if(tmp==3||tmp==6||tmp==9){
-                answer+="R";
-                right = tmp;
-            }else {
-                if(tmp==0) tmp=11;
-                int leftdist = Math.abs(tmp-left)/3+Math.abs(tmp-left)%3;
-                int rightdist = Math.abs(tmp-right)/3+Math.abs(tmp-right)%3;
-                
-                if(leftdist<rightdist){
-                    answer+="L";
-                    left = tmp;
-                }else if(leftdist>rightdist){
-                    answer+="R";
-                    right = tmp;
-                }else{
-                    if(hand.equals("left")){
-                        answer+="L";
-                        left = tmp;
-                    }else{
-                        answer+="R";
-                        right = tmp;
-                    }
-                }
-            }
+        for (int num : numbers) {
+            String Umji = pushNumber(num);
+            answer += Umji;
+
+            if(Umji.equals("L")) {leftPos = numpadPos[num]; continue;}
+            if(Umji.equals("R")) {rightPos = numpadPos[num]; continue;}
         }
         return answer;
+    }
+
+    //num버튼을 누를 때 어디 손을 사용하는가
+    private String pushNumber(int num) {
+        if(num==1 || num==4 || num==7) return "L";
+        if(num==3 || num==6 || num==9) return "R";
+
+        // 2,5,8,0 일때 어디 손가락이 가까운가
+        if(getDist(leftPos, num) > getDist(rightPos, num)) return "R";
+        if(getDist(leftPos, num) < getDist(rightPos, num)) return "L";
+
+        //같으면 손잡이
+        return this.hand;
+    }
+
+    //해당 위치와 번호 위치의 거리
+    private int getDist(int[] pos, int num) {
+        return Math.abs(pos[0]-numpadPos[num][0]) + Math.abs(pos[1]-numpadPos[num][1]);
     }
 }
