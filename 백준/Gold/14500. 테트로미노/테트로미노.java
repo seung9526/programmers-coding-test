2efda5a3d95ebad8fef7066e8eff828bf23daa8c
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-05-12 18:07:30 +0900

    [Gold IV] Title: 테트로미노, Time: 348 ms, Memory: 29412 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,92 +1,78 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
- 
-class Main {
-    static int N;
-    static int M;
-    static int[][] arr;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+    static int[][] graph;
     static boolean[][] visited;
-    static BufferedReader br;
-    static int[] dx = { -1, 0, 1, 0 };
-    static int[] dy = { 0, -1, 0, 1 };
-    static int max;
- 
+    static int n, m;
+    static int ans, max;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
     public static void main(String[] args) throws Exception {
- 
-        br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        N = Integer.parseInt(str[0]);
-        M = Integer.parseInt(str[1]);
-        arr = new int[N][M];
-        visited = new boolean[N][M];
-        max = 0;
-        for (int i = 0; i < N; i++) {
-            str = br.readLine().split(" ");
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(str[j]);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = Integer.parseInt(st.nextToken());
+                max = Math.max(max, graph[i][j]);
             }
         }
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
- 
-                DFS(i, j, 0, 0);
-                Exception(i, j);
- 
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dfs(j, i, graph[i][j], 1);
             }
         }
-        System.out.println(max);
+
+        System.out.println(ans);
+
+        br.close();
     }
-    //상하좌우 가능한 모형들 (ㅗ 모양 제외)
-    //'ㅗ' 모양은 DFS로 구현 불가
-    public static void DFS(int x, int y, int depth, int sum) {
- 
-        if (depth == 4) {
-            max = Math.max(max, sum);
+
+    static void dfs(int x, int y, int total, int cnt) {
+        if (cnt == 4) {
+            ans = Math.max(ans, total);
             return;
         }
- 
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
- 
-            if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
-                continue;
-            }
-            if (visited[nextX][nextY]) {
-                continue;
-            }
-            visited[nextX][nextY] = true;
-            DFS(nextX, nextY, depth + 1, sum + arr[nextX][nextY]);
-            visited[nextX][nextY] = false;
- 
+
+        if (ans >= total + max * (4 - cnt)) {
+            return;
         }
- 
+
+        visited[y][x] = true;
+
+        for (int k = 0; k < 4; k++) {
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
+            if (visited[ny][nx]) continue;
+
+            if (cnt == 2) {
+                visited[ny][nx] = true;
+                dfs(x, y, total + graph[ny][nx], cnt + 1);
+                visited[ny][nx] = false;
+                visited[y][x] = true;
+            }
+
+            dfs(nx, ny, total + graph[ny][nx], cnt + 1);
+        }
+
+        visited[y][x] = false;
     }
-    //'ㅗ' 모양 구현
-    //간단한 원리로는 + 모양에서 하나를 뺀다.
-    public static void Exception(int x, int y) {
-        int wing = 4;    // 가운데에서의 상하좌우 날개
-        int min = Integer.MAX_VALUE;
-        int sum = arr[x][y];
-        for (int i = 0; i < 4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
- 
-            //날개가 2개이상 없다면 ㅗ 모양이 아니다. 그러므로 함수를 종료한다.
-            if (wing <= 2)
-                return;
-            //날개가 맵 바깥에 있다면 날개가 아니다.
-            if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
-                wing--;
-                continue;
-            }
-            min = Math.min(min, arr[nextX][nextY]);
-            sum = sum + arr[nextX][nextY];
-        }
-        //날개가 4개일때 가장 작은 날개를 없애야 ㅗ,ㅏ,ㅓ,ㅜ 모양이 나온다.
-        if (wing == 4) {
-            sum = sum - min;
-        }
-        max = Math.max(max, sum);
-    }
+
+
 }
