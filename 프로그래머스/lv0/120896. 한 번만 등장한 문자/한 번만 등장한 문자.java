f20b3614b8aed5f593445c42c762e3ff3bcd596f
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-05-13 23:56:44 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 2.26 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,39 +1,26 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String solution(String s) {
         String answer = "";
-        char[] array=s.toCharArray();
-        int count=0;
-        
-        Arrays.sort(array);//a..z순으로 정렬
-        
-        s=String.valueOf(array);
-        
-        for(int i=0;i<s.length();i++){
-            if(i==s.length()-1){
-                if(count==0){
-                    answer=answer+s.charAt(i);
-                }
-                else{
-                    break;
-                }
-            }
-            else if(s.charAt(i)==s.charAt(i+1)){//뒤랑 같으면
-                count++;
-            }
-            else{//다르면
-                if(count>0){//근데 이전까지 같은애가 있었다.
-                    count=0;
-                    continue;
-                }
-                else{//같은 애도 없었고, 혼자다
-                    answer=answer+s.charAt(i);
-                }
-            }
+
+        String strTmp = s;
+        int sLength = strTmp.length();
+
+        List<String> charList = new ArrayList<>();
+        while(sLength > 0) {
+            String str = strTmp.substring(0,1);
+            strTmp = strTmp.replace(str, "");
+            if(sLength - strTmp.length() == 1){ charList.add(str); }
+            sLength = strTmp.length();
         }
-        
-        
+
+        Collections.sort(charList);
+
+        for(int i=0; i<charList.size(); i++){
+            answer += charList.get(i);
+        }
+
         return answer;
     }
 }
