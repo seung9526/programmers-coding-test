4794ca3b5fcf1c063a9543b4ccae46725e7666ff
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-05-15 19:37:01 +0900

    [Gold IV] Title: 테트로미노, Time: 340 ms, Memory: 29264 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -1,78 +1,88 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
 
-    static int[][] graph;
-    static boolean[][] visited;
-    static int n, m;
-    static int ans, max;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+	static int N, M; // 세로 N, 가로 M
+	static int[][] board;
+	static boolean[][] mainVisited;
+	static boolean[][] visited;
+	static int max = -1;
+	static final int size = 4;
+	static int[] dr = {0, -1, 0, 1};
+	static int[] dc = {1, 0, -1, 0};
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 세로 사이즈 입력
+		M = Integer.parseInt(st.nextToken()); // 가로 사이즈 입력
+		board = new int[N][M]; // 격자판 입력
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < M; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		visited = new boolean[N][M];
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < M; j++) {
+				visited[i][j] = true; // 방문처리
+				dfs(i, j, 1, board[i][j]); // 해당 좌표에서 
+				visited[i][j] = false; // 방문처리
+			}
+		}
+		System.out.println(max);
+	}
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        graph = new int[n][m];
-        visited = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                max = Math.max(max, graph[i][j]);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                dfs(j, i, graph[i][j], 1);
-            }
-        }
-
-        System.out.println(ans);
-
-        br.close();
-    }
-
-    static void dfs(int x, int y, int total, int cnt) {
-        if (cnt == 4) {
-            ans = Math.max(ans, total);
-            return;
-        }
-
-        if (ans >= total + max * (4 - cnt)) {
-            return;
-        }
-
-        visited[y][x] = true;
-
-        for (int k = 0; k < 4; k++) {
-            int nx = x + dx[k];
-            int ny = y + dy[k];
-
-            if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-            if (visited[ny][nx]) continue;
-
-            if (cnt == 2) {
-                visited[ny][nx] = true;
-                dfs(x, y, total + graph[ny][nx], cnt + 1);
-                visited[ny][nx] = false;
-                visited[y][x] = true;
-            }
-
-            dfs(nx, ny, total + graph[ny][nx], cnt + 1);
-        }
-
-        visited[y][x] = false;
-    }
-
-
+	private static void dfs(int i, int j, int c, int v) { // x, y좌표, 테트로미노 개수, 계산값
+		// 가지치기 : 앞으로 최대값만 발생해도 현재 max값을 넘지 못한다면 하지마라
+		if((v + 1000 *(size - c)) < max) return;
+		
+		// 기저 조건
+		if(c == 4) { // 테트로미노 완성
+			max = max < v ? v : max;
+			return;
+		}
+		
+		//실행
+		int nx, ny, nx2, ny2;
+		for(int d = 0; d < 4; d++) {
+			nx = i + dr[d];
+			ny = j + dc[d];
+			// 범위 체크
+			if(nx < 0 || nx >= N) continue;
+			if(ny < 0 || ny >= M) continue;
+			// 방문 체크
+			if(visited[nx][ny]) continue;
+			visited[nx][ny] = true;
+			// x, y좌표, 테트로미노 개수, 계산값
+			dfs(nx, ny, c+1, v+board[nx][ny]);
+			visited[nx][ny] = false;
+		}
+		if(c == 2) { // ㅗ 모양을 처리해준다
+			for(int d = 0; d < 4; d++) {
+				nx = i + dr[d];
+				ny = j + dc[d];
+				nx2 = i + dr[(d+1)%4];
+				ny2 = j + dc[(d+1)%4];
+				// 범위 체크
+				if(nx < 0 || nx >= N) continue;
+				if(ny < 0 || ny >= M) continue;
+				// 범위 체크
+				if(nx2 < 0 || nx2 >= N) continue;
+				if(ny2 < 0 || ny2 >= M) continue;
+				// 방문 체크
+				if(visited[nx][ny] || visited[nx2][ny2]) continue;
+				visited[nx][ny] = true;
+				visited[nx2][ny2] = true;
+				// x, y좌표, 테트로미노 개수, 계산값
+				dfs(nx, ny, c+2, v+board[nx][ny]+board[nx2][ny2]);
+				visited[nx][ny] = false;
+				visited[nx2][ny2] = false;
+			}
+		}
+	}
 }
