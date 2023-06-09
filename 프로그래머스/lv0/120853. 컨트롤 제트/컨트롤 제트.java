1051e0b9f8ba64c78ea116f433deb4b70d35ca4e
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-06-09 10:56:22 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.28 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for (String str : s.split(" ")){
+            if(str.equals("Z")){
+                stack.pop();
+            } else {
+                stack.push(Integer.parseInt(str));
+            }
+        }
+        
+        for(int i : stack){
+           answer += i;
+        }
+        return answer;
+        /*
         String[] sArr = s.split(" ");
         int answer = 0;
         
@@ -12,5 +30,6 @@ class Solution {
         }
         
         return answer;
+        */
     }
 }
