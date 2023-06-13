d4e55a70ed002c69c1060e4f9c7a504397ba5908
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-06-13 09:23:32 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/lv2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,4 +1,7 @@
--- 코드를 입력하세요
 SELECT
-    extract(hour from cast(DATETIME as timestamp)) as hour, count(*) as count from ANIMAL_OUTS 
-    where extract(hour from cast(DATETIME as timestamp)) between 9 and 19 group by extract(hour from cast(DATETIME as timestamp)) order by hour
+    extract(hour from cast(DATETIME as timestamp)) as hour, count(*) as count 
+    from ANIMAL_OUTS 
+    where extract(hour from cast(DATETIME as timestamp)) 
+    between 9 and 19 
+    group by extract(hour from cast(DATETIME as timestamp)) 
+    order by hour
