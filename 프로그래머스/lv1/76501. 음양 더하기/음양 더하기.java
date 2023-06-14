7cd96f4ceccec90a0398d779fed48fa7e0dbfcd6
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-06-14 18:41:12 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -3,6 +3,7 @@ class Solution {
         int answer = 0;
         
         for(int i=0; i<absolutes.length; i++){
+            // 3항 연산자 통해 1또는 -1을 곱해주어 결과 값 출력
             answer += absolutes[i] * (signs[i] ? 1 : -1);
             /*
             if(signs[i] == true){
