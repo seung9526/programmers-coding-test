934b030eb1240b64a43cef0191f10e484984035c
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-06-14 18:28:50 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0; i<absolutes.length; i++){
+            answer += (signs[i]) ? absolutes[i]*1 :-1*absolutes[i];
+            /*
+            if(signs[i] == true){
+                answer += absolutes[i];
+            } else if(signs[i] == false){
+                answer -= absolutes[i];
+            }
+            */
+        }
+        
+        return answer;
+    }
+}
