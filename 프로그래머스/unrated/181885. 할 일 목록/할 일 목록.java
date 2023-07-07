1e28ae0093a222bd701e56148326a5c8af781c89
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-07-07 10:50:49 +0900

    [unrated] Title: 할 일 목록, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+        
+        for(int i=0; i<finished.length; i++){
+            if(!finished[i]){
+                list.add(todo_list[i]);
+            }
+        }
+        
+        String[] strArr = list.toArray(new String[list.size()]);
+
+      
+        return strArr;
+    }
+}
