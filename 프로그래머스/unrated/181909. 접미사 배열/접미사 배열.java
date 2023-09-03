802a33d473d51c192a4df2c263d6ccbe4a17d457
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2023-09-03 22:40:32 +0900

    [unrated] Title: 접미사 배열, Time: 0.27 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        int n = my_string.length();
+        String[] answer = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            answer[i] = my_string.substring(i);
+        }
+        
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
