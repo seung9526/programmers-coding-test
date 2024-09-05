6e4ceb7a0d74418db159504ef8c2dd1b59bd2e66
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2024-09-05 22:25:01 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.go
@@ -0,0 +1,12 @@
+func solution(angle int) int {
+    if angle == 180 {
+        return 4
+    } 
+    if angle > 90 {
+        return 3
+    } 
+    if angle == 90 {
+        return 2
+    }
+    return 1
+}
