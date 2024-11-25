1c06a30fe6c72b0cde3126a5e445e89bbf3c86da
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2024-11-26 00:05:21 +0900

    [level 1] Title: 소수 만들기, Time: 4.76 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,26 @@
+function solution(nums) {
+    function isPrime(num) {
+        if(num < 2) return false;
+        for (let i = 2; i<= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        
+        return true;
+    }
+    
+    let count = 0;
+    
+    // 완전탐색
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                if (isPrime(sum)) {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
