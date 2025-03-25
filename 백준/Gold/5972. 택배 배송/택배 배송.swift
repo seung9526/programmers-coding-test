39a78188dcacc13ae07e2f69368164a70a72bdfa
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2025-03-25 22:42:13 +0900

    [Gold V] Title: 택배 배송, Time: 148 ms, Memory: 85248 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.swift
+++ 백준/Gold/5972. 택배 배송/택배 배송.swift
@@ -1,41 +1,58 @@
 import Foundation
 
-func solution(){
-    var input = readLine()!.split(separator: " ").map{Int(String($0))!}
+func solution() {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
     let N = input[0]
     let M = input[1]
-    var d = Array(repeating: Int.max, count: N)
-    d[0] = 0
-    var map = Array(repeating: [(Int,Int)](), count: N)
- 
-    for _ in 0..<M{
-        input = readLine()!.split(separator: " ").map{Int(String($0))!}
-        map[input[0]-1].append((input[1]-1,input[2]))
-        map[input[1]-1].append((input[0]-1,input[2]))
+
+    var graph = [[(Int, Int)]](repeating: [], count: N)
+
+    for _ in 0..<M {
+        let path = readLine()!.split(separator: " ").map { Int($0)! }
+        let A = path[0] - 1
+        let B = path[1] - 1
+        let C = path[2]
+        graph[A].append((B, C))
+        graph[B].append((A, C))
     }
 
-		// 두 헛간 사이의 길이 하나 이상일 수도 있음
-    for i in 0..<N{
-        map[i].sort(by: {$0.1 < $1.1})
+    // **탐색 순서 최적화: 간선 정렬 (가중치가 작은 순)**
+    for i in 0..<N {
+        graph[i].sort { $0.1 < $1.1 }
     }
+
+    var distances = [Int](repeating: Int.max, count: N)
+    distances[0] = 0
+
+    var queue = [(Int, Int)]()
+    queue.reserveCapacity(N)
+    queue.append((0, 0))
     
-    var queue = [Int]()
-    queue.append(0) // 시작 정점 0
-    
-    while !queue.isEmpty{
-        var newQueue = [Int]()
-								
-        for q in queue{
-            for next in map[q]{
-                if d[next.0] > d[q] + next.1{
-                    d[next.0] = d[q] + next.1
-                    newQueue.append(next.0)
-                }
+    var queueIndex = 0
+
+    while queueIndex < queue.count {
+        let (current, currentCost) = queue[queueIndex]
+        queueIndex += 1
+
+        if currentCost > distances[current] { continue }
+
+        for (next, edgeCost) in graph[current] {
+            let newCost = currentCost + edgeCost
+
+            if newCost < distances[next] {
+                distances[next] = newCost
+                queue.append((next, newCost))
             }
         }
-        queue = newQueue
+
+        // **배열 접근 최적화 (메모리 재할당 최소화)**
+        if queueIndex > 1000 {
+            queue.removeFirst(queueIndex)
+            queueIndex = 0
+        }
     }
-    print(d[N-1])
+
+    print(distances[N - 1])
 }
 
 solution()
