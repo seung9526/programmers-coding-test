fa0d057dcdd8088e6f4bffa28598f23c2465d5b8
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2025-05-15 21:49:14 +0900

    [Gold IV] Title: 고냥이, Time: 12 ms, Memory: 70668 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.swift
+++ 백준/Gold/16472. 고냥이/고냥이.swift
@@ -1,33 +1,40 @@
 func solution() {
     let N = Int(readLine()!)!
-    let input = Array(readLine()!) // 바로 Character 배열로 변환
+    let translator = readLine()!.map { String ($0) }
+    
+    var alphabetCount = Array(repeating: 0, count: 26) // a~z
+    var count = 0 
+    
+    var result = 0
 
-    var alphabetCount = [Int](repeating: 0, count: 26)
-    var uniqueCount = 0
-    var maxLength = 0
+    var left = 0 
+    var right = 0
 
-    var left = 0
-
-    for right in 0..<input.count {
-        let rightIndex = Int(input[right].asciiValue! - Character("a").asciiValue!)
+    alphabetCount[Int(translator[left].unicodeScalars.first!.value) - 97] += 1
+    count += 1
+    
+    while right < translator.count - 1 {
+        right += 1
+        
+        let rightIndex = Int(translator[right].unicodeScalars.first!.value) - 97
         alphabetCount[rightIndex] += 1
-        if alphabetCount[rightIndex] == 1 {
-            uniqueCount += 1
+        
+        if alphabetCount[rightIndex] == 1 { 
+            count += 1
         }
-
-        while uniqueCount > N {
-            let leftIndex = Int(input[left].asciiValue! - Character("a").asciiValue!)
+        
+        while count > N {
+            let leftIndex = Int(translator[left].unicodeScalars.first!.value) - 97
             alphabetCount[leftIndex] -= 1
             if alphabetCount[leftIndex] == 0 {
-                uniqueCount -= 1
+                count -= 1
             }
             left += 1
         }
-
-        maxLength = max(maxLength, right - left + 1)
+        
+        result = max(result, right-left+1)
     }
-
-    print(maxLength)
+    print(result)
 }
 
 solution()
