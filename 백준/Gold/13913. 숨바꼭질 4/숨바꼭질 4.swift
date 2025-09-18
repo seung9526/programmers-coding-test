f3a0feca20578553d2091e9e75bdca0242317bdf
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2025-09-18 21:59:47 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 52 ms, Memory: 72052 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.swift
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.swift
@@ -1,42 +1,49 @@
 func solution() {
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-    let start = input[0]
-    let target = input[1]
+    let input = readLine()!.split(separator: " ").map { Int(String($0))! }
 
-    if start == target {
+    let N = input[0] // 수빈이 위치
+    let K = input[1] // 동생의 위치
+
+    if N == K {
         print(0)
-        print(start)
+        print(N)
         return
     }
 
-    var visited = Array(repeating: -1, count: 100_001)
-    var queue: ArraySlice<Int> = [start][...]  // BFS queue를 ArraySlice로
-    var depth = 0
+    var visited = Array(repeating: -1, count: 100001) // 이전 좌표 저장
+
+    var queue = [N]
+    var dept = 0
 
     while !queue.isEmpty {
-        depth += 1
-        let currentLevel = queue
-        queue = []  // 다음 레벨 준비
+        dept += 1
+        var temp = [Int]() // queue 갱신 목적
 
-        for cur in currentLevel {
-            for next in [cur - 1, cur + 1, cur * 2] {
-                if (0...100_000).contains(next), visited[next] == -1 {
-                    visited[next] = cur
-                    queue.append(next)
-                    if next == target {  // 목표 도착 시 바로 경로 출력
-                        print(depth)
-                        var path = [target]
-                        var pre = target
-                        while pre != start {
-                            pre = visited[pre]
-                            path.append(pre)
-                        }
-                        print(path.reversed().map { String($0) }.joined(separator: " "))
-                        return
-                    }
+        for cur in queue {
+            if cur == K {
+                print(dept-1)
+                var pre = K
+                var path = [pre] // 경로 저장
+
+                // 경로 출력을 위한 반복문
+                while pre != N, pre != -1 {
+                    pre = visited[pre]
+                    path.append(pre)
+                }
+
+                print(path.map{ String($0) }.reversed().joined(separator: " "))
+                return
+            }
+            let next = [cur - 1, cur + 1, cur * 2]
+
+            for n in next { // 이동할 좌표 조건 검사
+                if (0...100000).contains(n), visited[n] == -1 {
+                    temp.append(n)
+                    visited[n] = cur
                 }
             }
         }
+        queue = temp // queue 갱신
     }
 }
 
