9d5874b3817b74cc870f4dcfeaa21ff32fc74bbc
GeungMin Seung <38849788+seung9526@users.noreply.github.com>
2025-09-18 22:03:37 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 48 ms, Memory: 72836 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.swift
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.swift
@@ -1,8 +1,7 @@
 func solution() {
     let input = readLine()!.split(separator: " ").map { Int(String($0))! }
-
-    let N = input[0] // 수빈이 위치
-    let K = input[1] // 동생의 위치
+    let N = input[0]
+    let K = input[1]
 
     if N == K {
         print(0)
@@ -10,40 +9,37 @@ func solution() {
         return
     }
 
-    var visited = Array(repeating: -1, count: 100001) // 이전 좌표 저장
-
+    var visited = Array(repeating: -1, count: 100_001) // 이전 좌표 저장
     var queue = [N]
-    var dept = 0
+    var depth = 0
 
     while !queue.isEmpty {
-        dept += 1
-        var temp = [Int]() // queue 갱신 목적
+        depth += 1
+        var nextQueue = [Int]()
 
-        for cur in queue {
+        // lazy sequence 적용: 필요한 값만 필터링
+        queue.lazy.forEach { cur in
             if cur == K {
-                print(dept-1)
+                print(depth - 1)
+                var path = [K]
                 var pre = K
-                var path = [pre] // 경로 저장
-
-                // 경로 출력을 위한 반복문
                 while pre != N, pre != -1 {
                     pre = visited[pre]
                     path.append(pre)
                 }
-
-                print(path.map{ String($0) }.reversed().joined(separator: " "))
+                print(path.reversed().map(String.init).joined(separator: " "))
                 return
             }
-            let next = [cur - 1, cur + 1, cur * 2]
 
-            for n in next { // 이동할 좌표 조건 검사
-                if (0...100000).contains(n), visited[n] == -1 {
-                    temp.append(n)
+            [cur - 1, cur + 1, cur * 2].lazy
+                .filter { (0...100_000).contains($0) && visited[$0] == -1 }
+                .forEach { n in
+                    nextQueue.append(n)
                     visited[n] = cur
                 }
-            }
         }
-        queue = temp // queue 갱신
+
+        queue = nextQueue
     }
 }
 
